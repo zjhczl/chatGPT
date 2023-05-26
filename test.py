ae3c060ff5aecbe5fa5ff295389b97ef38f4d775
zjhczl <2728774085@qq.com>
2023-05-26 17:17:47 +0800

    ss

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import requests   #导包
+import requests  
 
 def chatGPT():
-        url="https://api.openai.com/v1/chat/completions" #拼接地址
-        #参数
+        url="https://api.openai.com/v1/chat/completions" 
+       
         body={
     "model": "gpt-3.5-turbo",
     "messages": [{ "role": "user", "content": "你好" }],
@@ -12,9 +12,9 @@ def chatGPT():
         'Content-Type': 'application/json',
         'Authorization': 'Bearer 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'
     }
-        #发送请求
+       
         r=requests.post(url=url,data=body,headers=headers)
-        #输出返回
+      
         print(r.text)
-# print(getIp())
+
 chatGPT()
