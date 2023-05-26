dcee3cf81566ac5c189240faad10d5fe89ce2923
zjhczl <2728774085@qq.com>
2023-05-26 17:22:04 +0800

    ss

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ def chatGPT():
        
         body={
     "model": "gpt-3.5-turbo",
-    "messages": [{ "role": "user", "content": "你好" }],
+    "messages": [{ "role": "user", "content": "hello" }],
     "temperature": 0.7
 }
         headers ={
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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'
+        'Authorization': 'Bearer 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'
     }
        
         r=requests.post(url=url,data=body,headers=headers)
