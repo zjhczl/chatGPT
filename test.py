93f4b59baf5888f55eb76d25f9532eb89ee1cd5e
zjhczl <2728774085@qq.com>
2023-05-29 14:56:45 +0800

    s

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
 import requests  
 
-def chatGPT():
-        url="http://127.0.0.1:3000" 
+# def chatGPT():
+#         url="http://127.0.0.1:3000" 
        
-        body={
-    "model": "gpt-3.5-turbo",
-    "messages": [{ "role": "user", "content": "hello" }],
-    "temperature": 0.7
-}
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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'
-    }
+#         body={
+#     "model": "gpt-3.5-turbo",
+#     "messages": [{ "role": "user", "content": "hello" }],
+#     "temperature": 0.7
+# }
+#         headers = {
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+#         'Content-Type': 'application/json',
+#         'Authorization': 'Bearer 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'
+#     }
        
-        r=requests.post(url=url,data=body,headers=headers)
+#         r=requests.post(url=url,data=body,headers=headers)
       
-        print(r.text)
+#         print(r.text)
 
-chatGPT()
+# chatGPT()
+import os
+import openai
+openai.organization = "org-2Rtu2f0o58MCeWGZNfE2MpHw"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
