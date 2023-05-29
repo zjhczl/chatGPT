074037df4a2b8eaab352dd6d175a0b55201ad07d
zjhczl <2728774085@qq.com>
2023-05-29 15:10:43 +0800

    ss

--- test.py
+++ test.py
@@ -21,9 +21,14 @@ import requests
 # chatGPT()
 import os
 import openai
-openai.organization = "org-2Rtu2f0o58MCeWGZNfE2MpHw"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-prompt = 'Once upon a time'
-response = openai.Completion.create(model="text-davinci-003",prompt=prompt, temperature=0.5, max_tokens=100)
-print(response)
+r = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+print(r)
