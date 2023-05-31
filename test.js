f56ae6965c3e9dc3eaba5db1fd364801a312aae4
zjhczl <2728774085@qq.com>
2023-05-31 15:43:15 +0800

    s

--- test.js
+++ test.js
@@ -19,30 +19,47 @@
 //     .catch(error => {
 //         console.error(error);
 //     });
+// const axios = require('axios');
+// const url = 'https://api.openai.com/v1/chat/completions';
+
+// const config = {
+//     headers: {
+//         'content-type': 'application/json',
+//         'Authorization': 'Bearer ' + "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",
+//         'user-agent': 'PostmanRuntime/7.32.2',
+//         accept: '*/*',
+//         'postman-token': 'f746a838-7918-4f05-a1ca-e9c5ef4f1610',
+//         connection: 'keep-alive',
+//     }
+// };
+
+// const data = {
+//     "model": "gpt-3.5-turbo",
+//     "messages": [{ "role": "user", "content": "我是谁" }],
+//     "temperature": 0.7
+// };
+
+// axios.post(url, data, config)
+//     .then((response) => {
+//         console.log(response.data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 const axios = require('axios');
-const url = 'https://api.openai.com/v1/chat/completions';
 
-const config = {
-    headers: {
-        'content-type': 'application/json',
-        'Authorization': 'Bearer ' + "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",
-        'user-agent': 'PostmanRuntime/7.32.2',
-        accept: '*/*',
-        'postman-token': 'f746a838-7918-4f05-a1ca-e9c5ef4f1610',
-        connection: 'keep-alive',
-    }
-};
-
-const data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{ "role": "user", "content": "我是谁" }],
-    "temperature": 0.7
-};
-
-axios.post(url, data, config)
-    .then((response) => {
-        console.log(response.data);
+axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        prompt: 'Translate the following English text to French: 你好',
+        max_tokens: 60
+    }, {
+        headers: {
+            'Authorization': 'Bearer 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',
+            'Content-Type': 'application/json'
+        }
     })
-    .catch((error) => {
-        console.log(error);
+    .then(response => {
+        console.log(response.data.choices[0].text.trim());
+    })
+    .catch(error => {
+        console.error(error);
     });
