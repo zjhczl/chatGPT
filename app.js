06db42e8ef0e4c3d7cd1df7fee61e7a3b310ea6e
zjhczl <2728774085@qq.com>
2023-06-01 16:41:39 +0800

    s

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require("cors");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 
 const app = express();
 app.use(cors());
 
-// app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 
 // 定义路由和处理程序
