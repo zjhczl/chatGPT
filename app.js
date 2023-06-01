e3b05bf749cb210210d5a4f07156ba969f4a2be7
zjhczl <2728774085@qq.com>
2023-06-01 15:00:32 +0800

    s

--- app.js
+++ app.js
@@ -54,11 +54,29 @@ app.post('/', (req, res) => {
 });
 
 
-app.post('/test', (req, res) => {
-    // res.send(req.headers);
-    r = tlak()
+app.post('/test', async(req, res) => {
 
-    res.send(r);
+    var data = JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": "你好" //messages就是你发的消息是数组形式
+    });
+    var config = {
+        method: 'post',
+        url: 'https://api.openai-proxy.com/v1/chat/completions',
+        headers: {
+            'Authorization': 'Bearer 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',
+            'Content-Type': 'application/json',
+        },
+        data: data
+    };
+    axios(config)
+        .then(function(response) {
+            // console.log(JSON.stringify(response.data));
+            res.send({ code: 200, msg: response.data })
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 });
 
 
