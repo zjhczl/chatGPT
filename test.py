d09c1f9416d0e3da7c95bdb0152a037cdd828990
zjhczl <2728774085@qq.com>
2023-06-01 15:22:35 +0800

    s

--- test.py
+++ test.py
@@ -32,41 +32,41 @@ def talk(message):
         ]
     )
     print(r['choices'][0]['message']['content'])
-# message = sys.argv[1]
-# print(message)
-# talk(message)
+message = sys.argv[1]
+print(message)
+talk(message)
 
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
-def api():
-    data = request.get_json() # 获取POST请求中的数据
-    # 在这里编写你的业务逻辑，处理数据
-    message = data["messages"][0]["content"]
-    response = {
-    "id": "chatcmpl-7M9toeq4Bip79S0Zw77EON5jjayry",
-    "object": "chat.completion",
-    "created": 1685515624,
-    "model": "gpt-3.5-turbo-0301",
-    "usage": {
-        "prompt_tokens": 12,
-        "completion_tokens": 27,
-        "total_tokens": 39
-    },
-    "choices": [
-        {
-            "message": {
-                "role": "assistant",
-                "content": message
-            },
-            "finish_reason": "stop",
-            "index": 0
-        }
-    ]
-} # 构建响应数据
-    return jsonify(response) # 返回响应数据
+# @app.route('/api', methods=['POST'])
+# def api():
+#     data = request.get_json() # 获取POST请求中的数据
+#     # 在这里编写你的业务逻辑，处理数据
+#     message = data["messages"][0]["content"]
+#     response = {
+#     "id": "chatcmpl-7M9toeq4Bip79S0Zw77EON5jjayry",
+#     "object": "chat.completion",
+#     "created": 1685515624,
+#     "model": "gpt-3.5-turbo-0301",
+#     "usage": {
+#         "prompt_tokens": 12,
+#         "completion_tokens": 27,
+#         "total_tokens": 39
+#     },
+#     "choices": [
+#         {
+#             "message": {
+#                 "role": "assistant",
+#                 "content": message
+#             },
+#             "finish_reason": "stop",
+#             "index": 0
+#         }
+#     ]
+# } # 构建响应数据
+#     return jsonify(response) # 返回响应数据
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
